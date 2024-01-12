df660c2d2de511837d1fde3dbc7476a7883dc1f5
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-12 14:50:06 +0000

    Fri, Jan 12, 2024, 2:50 PM +00:00

--- Problems/Week_0/tip_calc/tip.py
+++ Problems/Week_0/tip_calc/tip.py
@@ -1,13 +1,13 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input("What percentage would you like to tip? "))
+#    percent = percent_to_float(input("What percentage would you like to tip? "))
 #    tip = dollars * percent
 #    print(f"Leave ${tip:.2f}")
 
 
 def dollars_to_float(d):
-    print(type(d))
-
+     str(d).rstrip("$")
+     print(d)
 
 def percent_to_float(p):
     print(p)

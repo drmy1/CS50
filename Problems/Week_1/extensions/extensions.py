a3434ee6f947a48d697924cd4dbb1f3b1b06ec59
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-13 17:38:24 +0000

    Sat, Jan 13, 2024, 6:38 PM +01:00

--- Problems/Week_1/extensions/extensions.py
+++ Problems/Week_1/extensions/extensions.py
@@ -1,1 +1,24 @@
-type = input()
+type = input("File name:\n").lower().strip()
+
+endlist = type.split(".")
+
+end = endlist[1]
+
+x = ""
+for i in range(len(end)):
+    if i <3:
+        x += end[i]
+
+match x:
+    case "jpg" | "jpeg" | "gif" | "png":
+        print(f"image/{x}")
+
+    case "pdf" | "zip":
+        print(f"application/{x}")
+
+    case "txt":
+        print("text/plain")
+    case _:
+        print ("application/octet-stream")
+
+

4ffb0adec3d6493477c2ce838d6f2f8921d548cc
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-13 17:49:25 +0000

    Sat, Jan 13, 2024, 6:49 PM +01:00

--- Problems/Week_1/extensions/extensions.py
+++ Problems/Week_1/extensions/extensions.py
@@ -4,26 +4,31 @@ type = input("File name:\n").lower().strip()
 
 endlist = type.split(".")
 
-if len(endlist) <= 1:
-    print ("application/octet-stream")
-else:
+if len(endlist) != 1:
+
     end = endlist[1]
     x = ""
 
     for i in range(len(end)):
         if i <3:
             x += end[i]
+        else:
+            break
 
-        match x:
-            case "jpg" | "jpeg" | "gif" | "png":
-                print(f"image/{x}")
+    match x:
+        case "jpg" | "jpeg" | "gif" | "png":
+            print(f"image/{x}")
 
-            case "pdf" | "zip":
-                print(f"application/{x}")
+        case "pdf" | "zip":
+            print(f"application/{x}")
 
-            case "txt":
-                print("text/plain")
-            case _:
-                print ("application/octet-stream")
+        case "txt":
+            print("text/plain")
+        case _:
+            print ("application/octet-stream")
+
+else:
+    print ("application/octet-stream")
+
 
 

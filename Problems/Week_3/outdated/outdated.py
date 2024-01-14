083e5957ad87dde54233e9b65d6a7b10378cfd79
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-14 19:56:37 +0000

    Sun, Jan 14, 2024, 8:56 PM +01:00

--- Problems/Week_3/outdated/outdated.py
+++ Problems/Week_3/outdated/outdated.py
@@ -1,48 +1,46 @@
-month = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+def main():
+    month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-while True:
-    try:
-        date = input("Date: ").split("/")
+    ydm = transform(month)
 
-        m = str(date[0])
-        day = date[1]
-        year = date[2]
+    print(f"{ydm[0]}-{ydm[1]}-{ydm[2]}")
 
-        if len(day) == 1:
-            day = "0"+day
 
-        if m.isalpha() == True:
-            try:
-                if m in month:
-                    m = str((month.index(m)) + 1)
-                    pass
-            except:
-                break
-        else:
+
+def transform(month):
+
+    while True:
+        try:
+            date = input("Date: ").split("/")
+
+            m = str(date[0])
+            day = date[1]
+            year = date[2]
+
+            if len(day) == 1:
+                day = "0"+day
+
+            if m.isalpha() == True:
+                try:
+                    if m in month:
+                        m = str((month.index(m)) + 1)
+                        pass
+                except:
+                    break
+            else:
+                pass
+
+            if len(m) == 1:
+                m = "0"+m
+
+            if m.isalpha() == False:
+                return year, m, day
+            else:
+                pass
+
+
+        except ValueError:
             pass
 
-        if len(m) == 1:
-            m = "0"+m
-
-        if m.isalpha() == False:
-            print(f"{year}-{m}-{day}")
-        else:
-            pass
-
-
-    except ValueError:
-        pass
-
+if __name__ == "__main__":
+    main()

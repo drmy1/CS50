82fab5d9120238e228103e42fe97bfb6d5645678
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-14 17:51:44 +0000

    Sun, Jan 14, 2024, 6:51 PM +01:00

--- Problems/Week_3/fuel/fuel.py
+++ Problems/Week_3/fuel/fuel.py
@@ -1,13 +1,13 @@
 def main():
     xy = int(calc("Fraction: "))
-    
+
     if xy >= 99:
         print("F")
     elif xy <= 1:
         print("E")
     else:
         print(f"{xy}%")
-    
+
 def calc(prompt):
     xy = []
     while True:
@@ -15,12 +15,12 @@ def calc(prompt):
             xy = str(input(prompt)).split("/")
             x = int(xy[0])
             y = int(xy[1])
-            
+
             if x > y:
                 calc(prompt)
-            
-            terminal = ((x / y) * 100)
-                
+
+            terminal = round((x / y) * 100)
+
             return terminal
         except ValueError:
             pass

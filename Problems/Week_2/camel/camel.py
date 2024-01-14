a5b5d635230d39baa68631eb0c662737df67bbb3
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-14 11:25:32 +0000

    Sun, Jan 14, 2024, 12:25 PM +01:00

---
+++ Problems/Week_2/camel/camel.py
@@ -0,0 +1,42 @@
+def main():
+    listc = listcut(input("camelCase:\n"))
+    index = separator(listc)
+
+    m = 0
+    snakecase = []
+    for k in range(len(listc)):
+        if k == index[m]:
+            snakecase.append("_"+ listc[k])
+
+        else:
+            snakecase.append(listc[k])
+    
+    output = "".join(snakecase).lower()
+    print(output)
+        
+    
+def listcut(camelCase):
+    
+    listcut = []
+    for i in range(len(camelCase)):
+        listcut.append(camelCase[i])
+    return listcut
+
+def separator(listcut):
+    index = []
+    for j in range(len(listcut)):
+        cut = str(listcut[j]).strip(",")
+        if cut.isupper() == True:
+            index.append(j)       
+        else:
+            pass
+    return index
+
+            
+    
+
+    
+    
+
+if __name__ == "__main__":
+    main()

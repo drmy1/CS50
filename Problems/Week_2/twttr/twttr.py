4831809d368618dc02faf940576dd41f1039f250
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-14 13:05:55 +0000

    Sun, Jan 14, 2024, 2:05 PM +01:00

---
+++ Problems/Week_2/twttr/twttr.py
@@ -0,0 +1,25 @@
+def main():
+    inputtext = input("Input: ")
+    outputtext = modify_text(inputtext)
+    
+def modify_text(text):
+    textlist = []
+    
+    for i in range(len(text)):
+        textlist.append(text[i])
+    
+    word = []
+    for k in range(len(textlist)):
+        match textlist[k]:
+            case "A" | "E" | "I" | "O" | "U" | "a" | "e" | "i" | "o" | "u":
+                pass
+            case _:
+                word.append(textlist[k])
+    
+    word = "".join(word)
+        
+    print(word)
+                
+
+if __name__ == "__main__":
+    main()

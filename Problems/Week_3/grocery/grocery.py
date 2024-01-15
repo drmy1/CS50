b11cbadf91b8c457e4d9f489ec45d2eeccd8411b
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-15 10:02:22 +0000

    Mon, Jan 15, 2024, 11:02 AM +01:00

---
+++ Problems/Week_3/grocery/grocery.py
@@ -0,0 +1,34 @@
+def main():
+
+    grocery_list = {}
+
+    grocery = item_list(grocery_list)
+
+def item_list(grocery_list):
+
+    while True:
+        try:
+            item = input().strip(" ")
+            if item  in grocery_list:
+                grocery_list[f"{item}"] += 1
+            else:
+                grocery_list[f"{item}"] = 1
+
+        except EOFError:
+            x = []
+            for val in grocery_list:
+                x.append(val)
+                for i in range(len(x)):
+                    z = x[i]
+                    z = str(z).strip("'")
+                print(grocery_list[f"{z}"], f"{z}")
+
+            pass
+        except KeyboardInterrupt:
+            pass
+
+
+
+
+if __name__ == "__main__":
+    main()

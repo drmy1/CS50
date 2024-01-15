3b45a52856798c1bf783468682caeb7f88e3002a
drmy1 <62697911+drmy1@users.noreply.github.com>
2024-01-15 08:10:58 +0000

    Mon, Jan 15, 2024, 9:10 AM +01:00

--- Problems/Week_3/outdated/outdated.py
+++ Problems/Week_3/outdated/outdated.py
@@ -2,54 +2,75 @@ def main():
     month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
     while True:
         try:
-            ydm = transform(month)
+            ydm = split(month)
+            print(ydm)
             print(f"{ydm[0]}-{ydm[1]}-{ydm[2]}", end="")
             break
-        except:
+        except KeyboardInterrupt:
             pass
-
-
-
-def transform(month):
-
-    while True:
-        try:
-            date = str(input("Date: "))
-
-            if "/" in date:
-                date = date.split("/")
-
-            if " " in date:
-                date = date.split(" ")
-
-            m = str(date[0])
-            day = date[1]
-            year = date[2]
-
-            if len(day) == 1:
-                day = "0"+day
-
-            if m.isalpha() == True:
-                try:
-                    if m in month:
-                        m = str((month.index(m)) + 1)
-                        pass
-                except:
-                    break
-            else:
-                pass
-
-            if len(m) == 1:
-                m = "0"+m
-
-            if m.isalpha() == False:
-                return year, m, day
-            else:
-                pass
-
-
         except ValueError:
             pass
+
+def split(month):
+
+
+    date = str(input("Date: "))
+
+
+    if "/" in date:
+        date = date.split("/")
+        m = str(date[0])
+        day = date[1]
+        year = date[2]
+
+        if m.isnumeric() == True:
+
+            if len(day) == 1:
+                if day.isnumeric() == True:
+                    day = "0" + day
+                else:
+                    pass
+
+            if len(m) == 1:
+                m = "0" + m
+                if year.isnumeric() == True:
+                    return year, m, day
+                else:
+                    pass
+
+
+
+
+    if " " in date:
+        date = date.split(" ")
+        m = str(date[0])
+        day = date[1]
+        year = date[2]
+
+        if m.isalpha() == True:
+            if "," in day:
+                day = day.strip(",")
+                if m in month:
+                    m = str((month.index(m)) + 1)
+                    if len(day) == 1:
+                        if day.isnumeric() == True:
+                            day = "0" + day
+                        else:
+                            pass
+
+                    if len(m) == 1:
+                        m = "0" + m
+                        if year.isnumeric() == True:
+                            return year, m, day
+                        else:
+                            pass
+                else:
+                    pass
+
+            else:
+                pass
+
+
 
 if __name__ == "__main__":
     main()
